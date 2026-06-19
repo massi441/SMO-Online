51917a522a1c912be9514b904ecf1334b3eee0fa
djaiz <djaizmassi@gmail.com>
2026-06-19 15:42:53 -0400

    Add dynamic repeated field attribute for computing dynamic sizes on required fields

---
+++ SMOO/Attributes/DynamicRepeatedFieldAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace SMOO.Attributes;
+
+[AttributeUsage(AttributeTargets.Field)]
+internal class DynamicRepeatedFieldAttribute : Attribute
+{
+    public required Type Type { get; init; }
+    public required int MaxRepeatCount { get; init; }
+}
