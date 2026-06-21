97f5e8c6889de0e4a041638c91949e00f8055a51
Massi Djaiz <djaizmassi@gmail.com>
2026-06-21 18:22:45 -0400

    Implement costume synchronization (#31)
    * Add helper function to create handler table entries
    
    * Implement costume synchronization event

--- SMOO/Event/EventChangeCostumeHandler.cs
+++ SMOO/Event/EventChangeCostumeHandler.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Logging;
+using SMOO.Attributes;
+using SMOO.Client;
 using SMOO.Protocol;
 using SMOO.Server;
 using SMOO.Util;
@@ -6,11 +9,54 @@ namespace SMOO.Event;
 
 internal class EventChangeCostumeHandler : IEventHandler
 {
-    public static ushort MinDataSize => 0;
-    public static ushort MaxDataSize => Config.MaxBufferSize;
+    public static ushort MinDataSize => RequiredSize<ChangeCostumeData>.MinSize;
+    public static ushort MaxDataSize => RequiredSize<ChangeCostumeData>.MaxSize;
+
+    private ref struct ChangeCostumeData : IDeserializableStruct
+    {
+        [DynamicField(MaxSize = Config.MaxCostumeNameLength)]
+        public StreamStringView<byte> CostumeName;
+
+        public void Deserialize(ref SpanReader reader)
+        {
+            CostumeName.Deserialize(ref reader, Config.MaxCostumeNameLength);
+        }
+    }
 
     public static void Handle(ParsedEventPacket packet, Room room, ServerContext context)
     {
-        
+        Player player = packet.BasePacket.SenderPlayer!;
+
+        try
+        {
+            ChangeCostumeData costumeData = PacketSerializer.Deserialize<ChangeCostumeData>(packet.EventData);
+
+            if (!costumeData.CostumeName.HasData())
+            {
+                PacketUtil.AckEvent(packet, context);
+                context.Logger.LogError("{PlayerName} sent an empty costume name in Room #{RoomId}", player.Name, room.Id);
+                return;
+            }
+
+            if (player.WorldInfo.CostumeBody == costumeData.CostumeName.String)
+            {
+                PacketUtil.AckEvent(packet, context);
+                context.Logger.LogInformation("{PlayerName} is already wearing the {CapName} costume, broadcast will be skipped", player.Name, costumeData.CostumeName);
+                return;
+            }
+
+            player.WorldInfo.CostumeBody = costumeData.CostumeName.String;
+
+            context.Logger.LogInformation("{PlayerName} is now wearing costume {CostumeName} in Room #{RoomId}", player.Name, costumeData.CostumeName, room.Id);
+
+            PacketUtil.AckEvent(packet, context);
+
+            room.Broadcaster.BroadcastReliably(packet.BasePacket.Buffer, room.Players.Except(player));
+        }
+        catch (InvalidDataException ex)
+        {
+            PacketUtil.AckEvent(packet, context);
+            context.Logger.LogInformation("{PlayerName} sent an invalid change costume packet in Room #{RoomId}: {Message}", player.Name, room.Id, ex.Message);
+        }
     }
 }
