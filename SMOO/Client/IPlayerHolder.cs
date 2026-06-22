e4dde72e8bab6bb9746e5db9441e10e3f3692c70
djaiz <djaizmassi@gmail.com>
2026-06-21 20:42:07 -0400

    Add read write lock to player holder

--- SMOO/Client/IPlayerHolder.cs
+++ SMOO/Client/IPlayerHolder.cs
@@ -7,6 +7,7 @@ internal interface IPlayerHolder
 {
     PlayerList Players { get; }
     byte MaxSize { get; }
+    ReaderWriterLockSlim ReadWriteLock { get; }
     ServerResult<Player> RegisterPlayer(in PlayerInfo playerInfo);
     ServerResult UnregisterPlayer(Player player);
     Player? FindPlayerByHost(IPEndPoint endpoint);
