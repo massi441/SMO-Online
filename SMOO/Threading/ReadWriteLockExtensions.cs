330a1995b0f4d5c9dcc473877cfd597ab9a23af3
djaiz <djaizmassi@gmail.com>
2026-06-21 20:13:54 -0400

    Add scoped lock utils

---
+++ SMOO/Threading/ReadWriteLockExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace SMOO.Threading;
+
+internal static class ReadWriteLockExtensions
+{
+    public static ScopedReadLock EnterReadScope(this ReaderWriterLockSlim readWriteLock)
+    {
+        return new ScopedReadLock(readWriteLock);
+    }
+
+    public static ScopedUpgradeableLock EnterUpgradeableScope(this ReaderWriterLockSlim readWriteLock)
+    {
+        return new ScopedUpgradeableLock(readWriteLock);
+    } 
+
+    public static ScopedWriteLock EnterWriteScope(this ReaderWriterLockSlim writeWriteLock)
+    {
+        return new ScopedWriteLock(writeWriteLock);
+    }
+}
