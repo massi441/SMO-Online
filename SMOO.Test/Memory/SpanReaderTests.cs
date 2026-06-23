8779968ac04c7601949c3fd64630e60fa7781ae1
djaiz <djaizmassi@gmail.com>
2026-06-22 22:28:59 -0400

    Add more SpanReader tests

--- SMOO.Test/Memory/SpanReaderTests.cs
+++ SMOO.Test/Memory/SpanReaderTests.cs
@@ -5,6 +5,15 @@ namespace SMOO.Test.Memory;
 
 public class SpanReaderTests
 {
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    private record struct PackedStruct
+    {
+        public required byte Byte;
+        public required int Int32;
+        public required float Single;
+        public required double Double;
+    }
+
     [Theory]
     [InlineData(0, 1, 2, 3, 4, 5)]
     [InlineData(10, 20, 5, -1, 8, -10)]
@@ -52,5 +61,69 @@ public class SpanReaderTests
 
         // Assert
         Assert.Equal(expectedByteCount, reader.RemainingByteCount);
+    }
+
+    [Theory]
+    [InlineData((byte)10, 232, 10.2363f, 350.5364d)]
+    [InlineData((byte)255, 1000000, -99.9999f, -1234.5678d)]
+    [InlineData((byte)0, -500, 0.0f, 0.0d)]
+    [InlineData((byte)127, -2147483648, 3.14159f, 999999.999d)]
+    [InlineData((byte)42, 12345, 2.71828f, 0.00001d)]
+    public void ReadGeneric_ReinterpretsBytes(byte byteData, int intData, float floatData, double doubleData)
+    {
+        // Arrange
+        PackedStruct expected = new PackedStruct()
+        {
+            Byte = byteData,
+            Int32 = intData,
+            Single = floatData,
+            Double = doubleData
+        };
+
+        ReadOnlySpan<byte> bytes = MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref expected, 1));
+
+        SpanReader reader = new SpanReader(bytes);
+
+        // Act
+        PackedStruct actual = reader.Read<PackedStruct>();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData((byte)10, 232, 10.2363f, 350.5364d)]
+    [InlineData((byte)255, 1000000, -99.9999f, -1234.5678d)]
+    [InlineData((byte)0, -500, -120.0f, 0.0d)]
+    [InlineData((byte)127, -2147483648, 3.14159f, 999999.999d)]
+    [InlineData((byte)42, 12345, 2.71828f, 0.00001d)]
+    public void ReadGeneric_ReadsByCopy(byte byteData, int intData, float floatData, double doubleData)
+    {
+        // Arrange
+        PackedStruct expected = new PackedStruct()
+        {
+            Byte = byteData,
+            Int32 = intData,
+            Single = floatData,
+            Double = doubleData
+        };
+
+        ReadOnlySpan<byte> bytes = MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref expected, 1));
+
+        SpanReader reader = new SpanReader(bytes);
+
+        // Act
+        PackedStruct actual = reader.Read<PackedStruct>();
+
+        expected.Byte = (byte)~expected.Byte;
+        expected.Int32 = ~expected.Int32;
+        expected.Single += 10.42f; 
+        expected.Double += 10.45d;
+
+        // Assert
+        Assert.NotEqual(expected.Byte, actual.Byte);
+        Assert.NotEqual(expected.Int32, actual.Int32);
+        Assert.NotEqual(expected.Single, actual.Single);
+        Assert.NotEqual(expected.Double, actual.Double);
     }
 }
