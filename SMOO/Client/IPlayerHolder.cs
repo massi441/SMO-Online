307033d30e2e22a0ba7fce6457326c7364da649f
djaiz <djaizmassi@gmail.com>
2026-08-22 14:40:07 -0400

    Remove locks from PlayerHolder

--- SMOO/Client/IPlayerHolder.cs
+++ SMOO/Client/IPlayerHolder.cs
@@ -7,7 +7,6 @@ internal interface IPlayerHolder
 {
     PlayerList Players { get; }
     byte MaxSize { get; }
-    ReaderWriterLockSlim ReadWriteLock { get; }
     ServerResult<Player> RegisterPlayer(in PlayerInfo playerInfo);
     ServerResult UnregisterPlayer(Player player);
     Player? FindPlayerByHost(IPEndPoint endpoint);
