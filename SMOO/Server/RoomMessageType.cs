435bebf412efa1e5d1eb5dd08bba5cbb5b156c1d
djaiz <djaizmassi@gmail.com>
2026-08-22 14:40:56 -0400

    Add Room Message and RoomMessageType

---
+++ SMOO/Server/RoomMessageType.cs
@@ -0,0 +1,22 @@
+﻿namespace SMOO.Server;
+
+/// <summary>
+/// Represents a type of "Message" that a room can process
+/// </summary>
+internal enum RoomMessageType : byte
+{
+    /// <summary>
+    /// A netowrk packet needs to be processed
+    /// </summary>
+    Packet,
+
+    /// <summary>
+    /// Reliable packets need to be checked and resent if needed
+    /// </summary>
+    PacketResend,
+
+    /// <summary>
+    /// Player idle statuses need to be checked
+    /// </summary>
+    PlayerHealthCheck,
+}
